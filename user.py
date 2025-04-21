8c3591c5ead3860305130e764400f136f3cac0be
yasin aliyari <yasinaliyari30@gmail.com>
2025-04-21 12:50:12 +0330

    create fullname function

--- user.py
+++ user.py
@@ -6,3 +6,7 @@ class User(BaseClass):
         self.last_name = last_name
         self.phone_number = phone_number
         super().__init__(*args, **kwargs)
+
+    @property
+    def fullname(self):
+        return f"{self.first_name,} {self.last_name}"

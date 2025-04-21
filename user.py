9c41b63607cf1438c9962faa9c2f9e95a517356d
yasin aliyari <yasinaliyari30@gmail.com>
2025-04-21 12:42:59 +0330

    Inheriting User from BaseClass

--- user.py
+++ user.py
@@ -1,4 +1,6 @@
-class User:
+from base import BaseClass
+
+class User(BaseClass):
     def __init__(self, first_name, last_name, phone_number, *args, **kwargs):
         self.first_name = first_name
         self.last_name = last_name

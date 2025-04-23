601d3bb3658cc7fed3b9f48e0a726c953b5274b1
yasin aliyari <yasinaliyari30@gmail.com>
2025-04-23 19:54:31 +0330

    create EstateAbstract class in estate.py

---
+++ estate.py
@@ -0,0 +1,12 @@
+from base import BaseClass
+from abc import Abc, abstractmethod
+
+class EstateAbstract(BaseClass):
+    def __init__(self, user, area, rooms_count, built_year, region, address, *args, **kwargs):
+        self.user = user
+        self.area = area
+        self.rooms_count = rooms_count
+        self.built_year = built_year
+        self.region = region
+        self.address = address
+        super().__init__(*args, **kwargs)

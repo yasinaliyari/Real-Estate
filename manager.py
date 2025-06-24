cc6fc69be8841a2b2ad75d22cad364d812c6d283
yasin aliyari <yasinaliyari30@gmail.com>
2025-06-24 20:31:27 +0330

    create search function

--- manager.py
+++ manager.py
@@ -1,3 +1,11 @@
 class Manager:
     def __init__(self, _class=None):
         self._class = _class
+
+    def search(self, **kwargs):
+        results = list()
+        for key, value in kwargs.items():
+            for obj in self._class.object_list:
+                if hasattr(obj, key) and getattr(obj, key) == value:
+                    results.append(obj)
+        return results

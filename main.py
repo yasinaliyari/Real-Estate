5a7d7696eec5d99aea88a517891e5366576a6ea0
yasin aliyari <yasinaliyari30@gmail.com>
2025-07-01 14:25:55 +0330

    create create_sample function in sample.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from sample import create_sample
 from advertisment import ApartmentSell, HouseSell, ApartmentRent, HouseRent
 
 if __name__ == "__main__":
+    create_sample()
--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ First_Name = ['yasin', 'Ali', 'Mehdi']
 Last_Name = ['Aliyari', 'Miri', 'Ajam']
 Mobiles = ['09123456789', '09876543219', '09625438756', '09621872988']
 
-if __name__ == "__main__":
+def create_sample():
     for mobile in Mobiles:
         User(choice(First_Name), choice(Last_Name), mobile)
 

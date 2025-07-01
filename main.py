5bdaabbf4de27d759a5c1ae907c5c647118ea9fe
yasin aliyari <yasinaliyari30@gmail.com>
2025-07-01 14:27:48 +0330

    create Handler class in main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from sample import create_sample
 from advertisment import ApartmentSell, HouseSell, ApartmentRent, HouseRent
 
+class Handler:
+    def run(self):
+        print('hello world')
+
 if __name__ == "__main__":
     create_sample()
+    handler = Handler()
+    
+    handler.run()
